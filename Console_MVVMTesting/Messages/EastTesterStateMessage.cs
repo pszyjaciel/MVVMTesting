03256ddd9dd5cd1d90e1cee0bbba2a77da68b1f8
Pawel Kramarz <pak@damm.dk>
2022-03-16 17:09:08 +0100

    added few class messages

---
+++ Console_MVVMTesting/Messages/EastTesterStateMessage.cs
@@ -0,0 +1,78 @@
+﻿using Console_MVVMTesting.Helpers;
+using Microsoft.Toolkit.Mvvm.Messaging.Messages;
+using System;
+
+namespace Console_MVVMTesting.Messages
+{
+    public enum ETStatus
+    {
+        Initialized,
+        Connected,
+        Disconnected,
+        Success,
+        Error,
+    }
+
+
+    public class EastTesterStateMessage
+    {
+        public string MyStateName { get; set; }
+        public ETStatus etStatus { get; set; }
+        public int ETErrorNumber { get; set; }
+
+
+        public EastTesterStateMessage(string myStateName, ETStatus ets)
+        {
+            MyUtils.MyConsoleWriteLine($"[{DateTime.Now.ToString("HH:mm:ss.ff")}]  ({this.GetHashCode():x8})");
+
+            MyStateName = myStateName;
+            etStatus = ets;
+        }
+
+        public EastTesterStateMessage(string myStateName)
+        {
+            MyUtils.MyConsoleWriteLine($"[{DateTime.Now.ToString("HH:mm:ss.ff")}] " +
+              $"MyUser::MyUser(1) " +
+              $"({this.GetHashCode():x8})");
+
+            MyStateName = myStateName;
+        }
+
+        public EastTesterStateMessage()
+        {
+            MyUtils.MyConsoleWriteLine($"[{DateTime.Now.ToString("HH:mm:ss.ff")}] " +
+              $"MyUser::MyUser(2) " +
+              $"({this.GetHashCode():x8})");
+        }
+
+        public ETStatus Response()
+        {
+            MyUtils.MyConsoleWriteLine($"[{DateTime.Now.ToString("HH:mm:ss.ff")}] MyStateMessage::Response()  ({this.GetHashCode():x8})");
+            return etStatus;
+        }
+    }
+
+
+    internal class EastTesterStatusRequestMessage : AsyncRequestMessage<EastTesterStateMessage>
+    {
+        public EastTesterStatusRequestMessage()
+        {
+            MyUtils.MyConsoleWriteLine($"[{DateTime.Now.ToString("HH:mm:ss.ff")}] EastTesterStatusRequestMessage::EastTesterStatusRequestMessage()  ({this.GetHashCode():x8})");
+        }
+    }
+
+
+    internal class EastTesterShutdownRequestMessage : RequestMessage<EastTesterStateMessage>
+    {
+        public EastTesterShutdownRequestMessage()
+        {
+            MyUtils.MyConsoleWriteLine($"[{DateTime.Now.ToString("HH:mm:ss.ff")}] EastTesterShutdownRequestMessage::EastTesterShutdownRequestMessage()  ({this.GetHashCode():x8})");
+        }
+
+    }
+
+
+
+
+
+}
--- Console_MVVMTesting/Messages/LCSocketStateMessage.cs
+++ Console_MVVMTesting/Messages/LCSocketStateMessage.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.Toolkit.Mvvm.Messaging.Messages;
+﻿using Console_MVVMTesting.Helpers;
+using Microsoft.Toolkit.Mvvm.Messaging.Messages;
+using System;
 using System.Net.Sockets;
 
 namespace Console_MVVMTesting.Messages
 {
-    public enum LCSocketStatusEnum
+    public enum LCStatus
     {
         Initialized,
         Connected,
@@ -12,15 +14,78 @@ namespace Console_MVVMTesting.Messages
         Error,
     }
 
-    public sealed class LCSocketStateMessage : AsyncRequestMessage<LCSocketStatusEnum>
+    public class LCSocketStateMessage
     {
-        public LCSocketStatusEnum LCStatus { get; }
+        public Socket mySocket { get; set; }
+        public string MyStateName { get; set; }
+        public LCStatus lcStatus { get; set; }
+        public int LCErrorNumber { get; set; }
 
-        public LCSocketStateMessage(LCSocketStatusEnum myStatus)
+
+        public LCSocketStateMessage(string myStateName, LCStatus lcs)
         {
-            System.Diagnostics.Debug.WriteLine($"LCSocketStateMessage::LCSocketStateMessage()");
-            LCStatus = myStatus;
+            MyUtils.MyConsoleWriteLine($"[{DateTime.Now.ToString("HH:mm:ss.ff")}]  ({this.GetHashCode():x8})");
+
+            MyStateName = myStateName;
+            lcStatus = lcs;
         }
 
+        public LCSocketStateMessage(string myStateName)
+        {
+            MyUtils.MyConsoleWriteLine($"[{DateTime.Now.ToString("HH:mm:ss.ff")}] " +
+              $"MyUser::MyUser(1) " +
+              $"({this.GetHashCode():x8})");
+
+            MyStateName = myStateName;
+        }
+
+        public LCSocketStateMessage()
+        {
+            MyUtils.MyConsoleWriteLine($"[{DateTime.Now.ToString("HH:mm:ss.ff")}] " +
+              $"MyUser::MyUser(2) " +
+              $"({this.GetHashCode():x8})");
+        }
+
+        //public LCStatus Response()
+        //{
+        //    MyUtils.MyConsoleWriteLine($"[{DateTime.Now.ToString("HH:mm:ss.ff")}] MyStateMessage::Response()  ({this.GetHashCode():x8})");
+        //    return lcStatus;
+        //}
+
     }
+
+
+    internal class LCSocketInitStatusRequestMessage : AsyncRequestMessage<LCSocketStateMessage>
+    {
+        public LCSocketInitStatusRequestMessage()
+        {
+            MyUtils.MyConsoleWriteLine($"[{DateTime.Now.ToString("HH:mm:ss.ff")}] LCSocketInitStatusRequestMessage::LCSocketInitStatusRequestMessage()  ({this.GetHashCode():x8})");
+        }
+    }
+
+    internal class LCSocketCheckPowerSupplyRequestMessage : AsyncRequestMessage<LCSocketStateMessage>
+    {
+        public LCSocketCheckPowerSupplyRequestMessage()
+        {
+            MyUtils.MyConsoleWriteLine($"[{DateTime.Now.ToString("HH:mm:ss.ff")}] LCSocketCheckPowerSupplyRequestMessage::LCSocketCheckPowerSupplyRequestMessage()  ({this.GetHashCode():x8})");
+        }
+    }
+
+
+    internal class LCShutdownRequestMessage : RequestMessage<LCSocketStateMessage>
+    {
+        public LCShutdownRequestMessage()
+        {
+            MyUtils.MyConsoleWriteLine($"[{DateTime.Now.ToString("HH:mm:ss.ff")}] LCShutdownRequestMessage::LCShutdownRequestMessage()  ({this.GetHashCode():x8})");
+        }
+    }
+
+    internal class LCSocketTestParameterRequestMessage : AsyncRequestMessage<LCSocketStateMessage>
+    {
+        public LCSocketTestParameterRequestMessage()
+        {
+            MyUtils.MyConsoleWriteLine($"[{DateTime.Now.ToString("HH:mm:ss.ff")}] LCSocketTestParameterRequestMessage::LCSocketTestParameterRequestMessage()  ({this.GetHashCode():x8})");
+        }
+    }
+
 }
